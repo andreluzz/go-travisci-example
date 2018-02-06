96ddb3cfc354fabdf446d79e121b6f6d2ddb3f88
André Luiz Mendonça <andreluzz@gmail.com>
2018-02-06 21:00:49 -0200

    test version

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"github.com/andreluzz/go-travisci-example/app"
 )
 
+var version = "Development"
+
 func main() {
 	for i := 0; i <= 15; i++ {
 		fmt.Println(app.FizzBuzz(i))
 	}
 	fmt.Println(app.ReadXml())
+	fmt.Printf("Version: %s", version)
 }
